4ba1d715484a7d5ec9f5695c5fc9e52f02365bdd
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2021-01-02 20:44:35 -0600

    :sparkles: (input) Adding checkbox component
    Adding checkbox component

---
+++ components/flowContent/FlowCheckbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input :id="id" type="checkbox" :name="name">
+    <label :for="id">
+      <slot />
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'FlowCheckbox',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
